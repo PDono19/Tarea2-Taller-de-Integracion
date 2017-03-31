f4614f88db4bf73648d410104d0de00a76ddab5c
PDono19 <pjdonoso@uc.cl>
2017-03-31 12:26:17 -0300

    10/10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,63 @@
 class UsersController < ApplicationController
+
   def index
-    @users = Users.all
-    render json: @users, status: :ok
+    @users = User.all
+    message = {'usuarios' => @users, 'total' => @users.count}.to_json
+    render json: message, status: :ok
   end
+
   def show
-    render jason: @user
+    @user = User.find(params[:id])
+    if @user != nil
+      render jason: @user, status: :ok
+    else
+      message = {'error' => 'Usuario no encontrado'}.to_json
+      render json: message, status: :not_found
+    end
   end
+
   def destroy
-    @user.destroy
-    head :no_content
-  end
-  def update
-    if @user.update(contact_params)
+    if @user != nil
+      @user.destroy
       head :no_content
     else
-      render json: @user.errors, status: :unprocessable_entity
+      message = {'error' => 'Usuario no encontrado'}.to_json
+      render json: message, status: :not_found
+    end
+  end
+
+  def update
+    if @user != nil
+      if @user.update(user_params)
+        render jason: @user, status: :ok
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    else
+      message = {'error' => 'Usuario no encontrado'}.to_json
+      render json: message, status: :not_found
     end
   end
 
   def create
-    @user = Contact.new(contact_params)
+    @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created
+    if params[:id].present?
+      message = {'error' => 'No se puede crear usuario con id'}.to_json
+      render json: message, status: :bad_request
     else
-      render json: @user.errors, status: :unprocessable_entity
+
+      if @user.save
+        render json: @user, status: :created
+      else
+        message = {'error' => 'La creación ha fallado'}.to_json
+        render json: message, status: :bad_request
+      end
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:usuario, :nombre, :apellido, :twitter)
   end
 
 end
